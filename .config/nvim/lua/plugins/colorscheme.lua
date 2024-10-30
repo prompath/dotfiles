4d3030043dbc0dab1aa2e45a3b5bef3b550a7488
Jeerawat Chuachan <jeerawatchu@gmail.com>
2024-10-30 08:45:10 +0700

    add tmux and nvim

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
